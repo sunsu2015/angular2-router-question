f6bcbd8b6099ede8db29b3e5d6e5ad499ecf2602
sunsu2015 <2435008@163.com>
2016-12-27 15:55:54 +0800

    增加menu里的内容

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: 'menu.component.html',
@@ -7,9 +7,18 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
-  constructor() { }
+  constructor(public router:Router) { }
 
   ngOnInit() {
   }
-
+  handleClick(e){
+    switch (e.target.innerText){
+      case 'm2':
+        this.router.navigate(['./home/m2']);
+        break;
+      case 'm3':
+        this.router.navigate(['./home/m3']);
+        break;
+    }
+  }
 }
